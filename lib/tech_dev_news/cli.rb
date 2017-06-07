a5606cd49290e3c6439b652af23a645df9b06676
Joe Kim <josephkim0224@gmail.com>
2017-06-07 07:39:03 -0400

    Implement greetings and list_news methods

--- lib/tech_dev_news/cli.rb
+++ lib/tech_dev_news/cli.rb
@@ -1,6 +1,26 @@
 class TechDevNews::CLI
 
   def call
-    puts "Temporary message"
+    greetings
+    list_news
+
   end
+
+  private
+
+    def greetings
+      # putting in a clear screen code I found few projects ago
+      puts "\e[H\e[2J"
+      puts "***********************************"
+      puts "Welcome to Tech Developer News CLI"
+      puts "***********************************"
+      puts ""
+    end 
+
+    def list_news
+      puts "Here are today's list of Tech Developer News"
+      # putting in a fake list for now to test the app
+      puts "1. What you need to know about iOS 12 - Ryan Daws - 06 June 2017"
+      puts "2. Apple developers break $90 billion earnings as Swift Playgrounds expands to educate the next generation - Ryan Daws - 02 June 2017"
+    end
 end
